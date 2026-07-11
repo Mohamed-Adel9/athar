8fb8c07d3261469e3249657d6c38dc2458157bc3
m.adel <eng.moaamed.adel49@gmail.com>
2026-07-11 13:36:44 +0300

    add api url

--- lib/core/const_data/api_urls.dart
+++ lib/core/const_data/api_urls.dart
@@ -1,7 +1,7 @@
 class ApiUrls {
   const ApiUrls._();
 
-  static const baseUrl = 'http://127.0.0.1:8000/api/';
+  static const baseUrl = 'http://10.0.2.2:9000/api/';
 
   static const login = 'auth/login';
   static const register = 'auth/register';
